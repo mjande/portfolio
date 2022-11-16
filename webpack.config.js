1abe25b0d55bf9f197527f2aa91e1f04e1171a81
mattand <mjande@umich.edu>
2022-11-16 10:11:07 -0500

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,4 +8,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "public"),
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./public",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
